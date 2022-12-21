9b6e1fd9b95ec44ddafc434f008d4612c79f600d
Kevin Zanao <kevinzanao2108@gmail.com>
2022-12-21 10:08:28 -0300

    code adjustment

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-alert("O formato da data está como Mês/Dia/Ano por causa do formato do input!");
+// bug to resolve: enter key send the object instead of saving
 
+// HTML elements
 const $form = document.getElementById('js-form');
 const $tbody = document.querySelector('#table tbody');
 const $name = document.getElementById('name');
@@ -11,7 +12,7 @@ const $saveButton = document.getElementById('save-button');
 
 // Input patterns
 const namePattern = /\D{2,119}([aA-zZ]+([ç]+)?)/;
-const birthDatePattern = '';
+const birthDatePattern = /\d{2}[/]\d{2}[/]\d{4}/;
 const isNumberPattern = /\d/;
 let inputNameConfirmation = false;
 let birthDateConfirmation = false;  
@@ -47,19 +48,20 @@ $birthDate.addEventListener('input', (e) => {
 
 // Check that date value are correct
 function birthDateValidation(date) {
-
   birthDateConfirmation = false;
 
   const year = new Date();
   
-  let formatedDate = date.split("-");
+  let formatedDate = date.split("/");
 
-  const yearLimit = formatedDate[0] <= year.getFullYear() && formatedDate[0] >= 1800;
+  const yearLimit = formatedDate[2] <= year.getFullYear() && formatedDate[2] >= 1800;
   const monthLimit = formatedDate[1] <= 12 && formatedDate[1] > 0;
-  const dayLimit = formatedDate[2] <= 31 && formatedDate[2] > 0;
+  const dayLimit = formatedDate[0] <= 31 && formatedDate[0] > 0;
 
   if (dayLimit && monthLimit && yearLimit) {
-    birthDateConfirmation = true;
+    if (birthDatePattern.test($birthDate.value)) {
+      birthDateConfirmation = true;
+    }
   }
 
   releaseToSend();
@@ -107,6 +109,39 @@ $form.addEventListener("submit", (e) => {
   e.preventDefault();
 });
 
+function createTableElements(value) {
+  // table elements
+  const tr = document.createElement('tr')
+  const td_name = document.createElement('td');
+  const td_birth = document.createElement('td');
+  const td_actions = document.createElement('td');
+  const editButton = document.createElement('img');
+  const deleteButton = document.createElement('img');
+
+  // Format birth date to BR type
+  let birthDateFormated = value.birthDate.split("/");
+  birthDateFormated.reverse()
+
+  tr.id = value.id;
+  td_name.innerText = value.name;
+  td_birth.innerText = birthDateFormated.join("/");
+  editButton.src = './assets/edit-icon.svg';
+  editButton.alt = 'edit icon';
+  editButton.addEventListener('click', editPersonData);
+  deleteButton.src = './assets/delete-icon.svg';
+  deleteButton.alt = 'delete icon';
+  deleteButton.addEventListener('click', deletePersonRegister);
+
+  td_actions.appendChild(editButton);
+  td_actions.appendChild(deleteButton);
+  tr.appendChild(td_name);
+  tr.appendChild(td_birth);
+  tr.appendChild(td_actions);
+  $tbody.appendChild(tr);
+}
+
+// FUNCTIONS GET VALUE
+
 // get person's data and create an array that returns person's Data and index position
 function getPersonData() {
   let element = event.target.parentNode;
@@ -124,114 +159,20 @@ function getPersonData() {
   return [findedPerson, storedPeoples.indexOf(findedPerson)]
 }
 
-// update the state of elements needed to edit the person's data
-function updateHtmlElementsState(person) {
-  $name.value = person.name;
-  $birthDate.value = person.birthDate;
-
-  nameRegexValidation();
-  birthDateValidation(person.birthDate);
-
-  $submit.hidden = true;
-  $saveButton.hidden = false;
-}
-
-// Stores last person the function got 
-let lastPersonDataUsed;
-
-// Update and save to localStorage
-function savePersonDataEdit() {
-  const data = lastPersonDataUsed;
-  const personData = data[0];
-  
-  const personIndexArray = data[1];
-  let storedPeoples = JSON.parse(localStorage.getItem("peoples"));
-  
-  personData.name = $name.value;
-  personData.birthDate = $birthDate.value;
-
-  storedPeoples[personIndexArray] = personData;
-  localStorage.setItem("peoples", JSON.stringify(storedPeoples));
-
-  $name.value = "";
-  $birthDate.value = "";
-  $saveButton.hidden = true;
-  $submit.hidden = false;
-  $submit.disabled = true;
-  inputNameConfirmation = false;
-  birthDateConfirmation = false;
-
-  getStoredPeoplesAndShow()
-}
-
-// Passes the data to update the HTML elements and to the function that saves person's edited data
-function editPersonData() {
-  const data = getPersonData();
-  lastPersonDataUsed = data;
-  const personData = data[0];
-
-  updateHtmlElementsState(personData);
-
-  $name.focus();
-}
-
-function deletePersonRegister() {
-  const data = getPersonData()
-  
-  let storedPeoples = JSON.parse(localStorage.getItem("peoples"));
-  
-  storedPeoples.splice(data[1], 1);
-  
-  localStorage.setItem("peoples", JSON.stringify(storedPeoples));
-
-  getStoredPeoplesAndShow();
-}
-
 // Create elements in the table and add the person's data inside of them
 function getStoredPeoplesAndShow() {
+  let storedPeoples = JSON.parse(localStorage.getItem("peoples"));
 
-  let storedPeoples = JSON.parse(localStorage.getItem("peoples"))
-  // console.log("Show peoples",storedPeoples);
-  const tbody = document.querySelector('#table tbody');
-  tbody.innerHTML = "";
+  $tbody.innerHTML = "";
 
   if (storedPeoples) {
-
-    storedPeoples.forEach((person => {
-  
-      const tr = document.createElement('tr')
-      const td_name = document.createElement('td');
-      const td_birth = document.createElement('td');
-      const td_actions = document.createElement('td');
-      const editButton = document.createElement('img');
-      const deleteButton = document.createElement('img');
-
-      // Format birth date to BR type
-      let birthDateFormated = person.birthDate.split("-");
-      birthDateFormated.reverse()
-
-      tr.id = person.id;
-      td_name.innerText = person.name;
-      td_birth.innerText = birthDateFormated.join("-");
-      editButton.src = './assets/edit-icon.svg';
-      editButton.alt = 'edit icon';
-      editButton.addEventListener('click', editPersonData);
-      deleteButton.src = './assets/delete-icon.svg';
-      deleteButton.alt = 'delete icon';
-      deleteButton.addEventListener('click', deletePersonRegister);
-  
-      td_actions.appendChild(editButton);
-      td_actions.appendChild(deleteButton);
-      tr.appendChild(td_name);
-      tr.appendChild(td_birth);
-      tr.appendChild(td_actions);
-      tbody.appendChild(tr);
-    }));
+    storedPeoples.forEach((person) => {
+      createTableElements(person);
+    });
   }
-} 
 
-$submit.onclick = getInputValues;
-$saveButton.onclick = savePersonDataEdit;
+  // enterKeyDownSubmit();
+} 
 
 // Takes input values and display in the console
 function getInputValues() {
@@ -250,6 +191,94 @@ function getInputValues() {
   $birthDate.value = '';
   $submit.disabled = true;
   $name.focus();
+}
+
+// Stores last person the editPersonData() got 
+let lastPersonDataUsed;
+
+// ACTION FUCTIONS
+
+// Update and save to localStorage
+function savePersonDataEdit() {
+  const data = lastPersonDataUsed;
+  const personData = data[0];
+  
+  const personIndexArray = data[1];
+  let storedPeoples = JSON.parse(localStorage.getItem("peoples"));
+  
+  personData.name = $name.value;
+  personData.birthDate = $birthDate.value;
+
+  storedPeoples[personIndexArray] = personData;
+  localStorage.setItem("peoples", JSON.stringify(storedPeoples));
+
+
+  $name.value = "";
+  $birthDate.value = "";
+  $saveButton.hidden = true;
+  $submit.hidden = false;
+  $submit.disabled = true;
+  inputNameConfirmation = false;
+  birthDateConfirmation = false;
+
+  getStoredPeoplesAndShow()
+}
+
+// update the state of elements needed to edit the person's data
+function updateHtmlElementsState(person) {
+  $name.value = person.name;
+  $birthDate.value = person.birthDate;
+
+  nameRegexValidation();
+  birthDateValidation(person.birthDate);
+
+  $submit.hidden = true;
+  $saveButton.hidden = false;
+}
+
+// Passes the data to update the HTML elements and to the function that saves person's edited data
+function editPersonData() {
+  const data = getPersonData();
+  lastPersonDataUsed = data;
+  const personData = data[0];
+
+  updateHtmlElementsState(personData);
+  // enterKeyDownSave();
+
+  $name.focus();
+}
+
+function deletePersonRegister() {
+  const data = getPersonData()
+  
+  let storedPeoples = JSON.parse(localStorage.getItem("peoples"));
+  
+  storedPeoples.splice(data[1], 1);
+  
+  localStorage.setItem("peoples", JSON.stringify(storedPeoples));
+
+  getStoredPeoplesAndShow();
+}
+
+$submit.onclick = getInputValues;
+$saveButton.onclick = savePersonDataEdit;
+
+function enterKeyDownSave() {
+  document.addEventListener('keydown', (event) => {
+    if (event.keyCode === 13) {
+      console.log("cliquei no enter ")
+      savePersonDataEdit();
+    }
+  })
+}
+
+function enterKeyDownSubmit() {
+  document.addEventListener('keydown', (event) => {
+    if (event.keyCode === 13) {
+      console.log("enter de submit")
+      getInputValues();
+    }
+  })
 }
 
 // Creates an array in local storage, and adds the "person" object inside
